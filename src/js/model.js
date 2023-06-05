b6192eda64712989d8b23955438e9f3f34e00c2b
mdefab <defab@live.ca>
2023-06-04 22:29:34 -0400

    Initial html/css for game screen, create player and item images, write BaseFighterClass methods

---
+++ src/js/model.js
@@ -0,0 +1,18 @@
+export const gameState = {
+    gameStats: {
+        playerOneWins: 0,
+        playerTwoWins: 0,
+    },
+
+    playerOne: {
+        health: 100,
+        stamina: 100,
+        itemsEquiped: [],
+    },
+
+    playerTwo: {
+        health: 100,
+        stamina: 100,
+        itemsEquiped: [],
+    }
+}
