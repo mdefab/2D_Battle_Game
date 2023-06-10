3981b02cf5e7f391ad61073743c619f2963b0353
mdefab <defab@live.ca>
2023-06-09 20:58:28 -0400

    Implement ready button functionality in GameView

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,15 +3,20 @@ import GameView from "./views/GameView.js";
 import * as model from "./model.js";
 
 
-//todo: pass data into model to initialize fighter class
 const controlStartButton = function(data){
     const gameState = model.initializeGame(data);
     GameView.updateScore(gameState.gameStats.playerOneScore, gameState.gameStats.playerTwoScore);
     GameView.updateHealthandStamina(gameState.playerOne, gameState.playerTwo);
 };
 
+//receives move choice. todo: pass choice into model to get result of move
+const controlReadyButton = function(data){
+    console.log(data);
+}
+
 const init = function(){
     FighterMenuView.addHandlerStartButton(controlStartButton);
+    GameView.addHandlerReadyButton(controlReadyButton);
 };
 
 init();
--- src/js/views/GameView.js
+++ src/js/views/GameView.js
@@ -3,11 +3,64 @@ class GameView {
     _scorePlayerTwo = document.querySelector('.score-player--two');
     _vitalsPlayerOne = document.querySelector('.vitals-player--one');
     _vitalsPlayerTwo = document.querySelector('.vitals-player--two');
+    _readyButtonOne = document.querySelector('.ready-one');
+    _readyButtonTwo = document.querySelector('.ready-two');
+    _gameMessageOne = document.querySelector('.game-message--one');
+    _gameMessageTwo = document.querySelector('.game-message--two');
+    _selectionBoxOne = document.querySelector('.selection--one');
+    _selectionBoxTwo = document.querySelector('.selection--two');
     _readyOne = false;
     _readyTwo = false;
-    
-    //eventlistener for ready button
+    _playerOneMove;
+    _playerTwoMove;
 
+    
+    //eventlistener for ready button. todo: make select-box disappear on ready.
+    addHandlerReadyButton(handler){
+        this._readyButtonOne.addEventListener("click", function(){
+            this._readyOne = true;
+            if(!this._readyTwo){
+                this._selectionBoxOne.classList.add('hidden');
+                this._gameMessageOne.insertAdjacentHTML('afterbegin', "Player 1 ready. <br> Waiting for player 2");
+                return; //other player not ready
+            }
+            this._readyOne = false;
+            this._readyTwo = false;
+            this._clearMessages();
+            this._showSelectionBox();
+            const setUpData = this._getPlayerMoves();
+            handler(setUpData);
+        }.bind(this))
+
+        this._readyButtonTwo.addEventListener("click", function(){
+            this._readyTwo = true;
+            if(!this._readyOne){
+                this._selectionBoxTwo.classList.add('hidden');
+                this._gameMessageTwo.insertAdjacentHTML('afterbegin', "Player 2 ready. <br> Waiting for player 1");
+                return; //other player not ready
+            }
+            this._readyOne = false;
+            this._readyTwo = false;
+            this._clearMessages();
+            this._showSelectionBox();
+            const setUpData = this._getPlayerMoves();
+
+            handler(setUpData);
+        }.bind(this))
+    }
+
+    _getPlayerMoves(){
+            const moveOne = document.getElementById('choice-player--one').value;
+            this._playerOneMove = moveOne === "random"? this._randomMoveGenerator(): moveOne;
+            const moveTwo = document.getElementById('choice-player--two').value;
+            this._playerTwoMove = moveTwo === "random"? this._randomMoveGenerator(): moveTwo;
+
+        return {
+            'playerOneMove': this._playerOneMove,
+            'playerTwoMove': this._playerTwoMove,
+        }
+    }
+    
 
 
     //method to update score on page. Should be called at start and after each game.
@@ -49,8 +102,17 @@ class GameView {
         return move
     }
 
-    // method to reset ready status between rounds
 
-};
+    //clear ready up messages, if any
+    _clearMessages(){
+        this._gameMessageOne.innerHTML = '';
+        this._gameMessageTwo.innerHTML = '';
+    }
+
+    //bring selection box back after hiding on ready
+    _showSelectionBox(){
+        this._selectionBoxOne.classList.remove('hidden');
+        this._selectionBoxTwo.classList.remove('hidden');
+    }};
 
 export default new GameView();
