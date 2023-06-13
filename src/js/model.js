36d6ceb6d9fe91cae8383cc58fd2e6592409683c
mdefab <defab@live.ca>
2023-06-13 19:04:19 -0400

    Add notes into model.fighterMoveResult todo later

--- src/js/model.js
+++ src/js/model.js
@@ -44,28 +44,48 @@ export const fighterMoveResult = function(move){
         const playerOneValue = moveValueCalculator(1, move.playerOneMove);
         const playerTwoValue = moveValueCalculator(2, move.playerTwoMove);
         //connect result of moves on each other.
+
         // 1) playerOne attack and playerTwo defend
+        console.log(playerOneValue);
+        console.log(playerTwoValue);
         if((playerOneValue.attack || playerOneValue.attack === 0) && playerTwoValue.defend){
             const damageTakenTwo = gameState.playerTwo.damageTaken(playerOneValue.attack, playerTwoValue.defend);
             console.log(`player Two stopped ${playerOneValue.attack - damageTakenTwo} damage and took ${damageTakenTwo} damage`);
         }
 
-        //2) playerOne attack and playerTwo attack or pick up item
-        if(playerOneValue.attack && (playerTwoValue.attack || playerTwoValue.item)){
-            const damageTakenTwo = gameState.playerTwo.damageTaken(playerOneValue.attack);
-            console.log(`player Two took ${damageTakenTwo} damage`);
-        }
-        // 3) playerTwo attack and playerOne defend
+        // 2) playerTwo attack and playerOne defend
         if((playerTwoValue.attack || playerTwoValue.attack === 0) && playerOneValue.defend){
             const damageTakenOne = gameState.playerOne.damageTaken(playerTwoValue.attack, playerOneValue.defend);
             console.log(`player One stopped ${playerTwoValue.attack - damageTakenOne} damage and took ${damageTakenOne} damage`);
         }
-        //4) playerTwo attack and playerOne attack or pick up item
-        if(playerTwoValue.attack && (playerOneValue.attack || playerOneValue.item)){
+
+        //3) both players attack
+        if((playerOneValue.attack || playerOneValue.attack === 0) && (playerTwoValue.attack || playerTwoValue.attack === 0)){
+            const damageTakenOne = gameState.playerOne.damageTaken(playerTwoValue.attack);
+            const damageTakenTwo = gameState.playerTwo.damageTaken(playerOneValue.attack);
+            console.log(`player One took ${damageTakenOne} damage`);
+            console.log(`player Two took ${damageTakenTwo} damage`);
+        }
+
+        //4) playerOne attack and playerTwo picks up an item
+        if((playerOneValue.attack || playerOneValue.attack === 0) && playerTwoValue.item){
+            const damageTakenTwo = gameState.playerTwo.damageTaken(playerOneValue.attack);
+            console.log(`player Two took ${damageTakenTwo} damage`);
+        }
+
+        //5) playerTwo attack and playerOne picks up item
+        if((playerTwoValue.attack || playerTwoValue.attack === 0) && playerOneValue.item){
             const damageTakenOne = gameState.playerOne.damageTaken(playerTwoValue.attack);
             console.log(`player One took ${damageTakenOne} damage`);
         }
 
+        //6) Both players defend
+    
+        // 7) PlayerOne defend and playerTwo item
+
+        //8) PlayerTwo defend and playerOne item
+
+        //Note: There is no Item vs Item because players do not affect each other and items applied automatically
         return {
             'playerOneMove':playerOneValue,
             'playerTwoMove': playerTwoValue,
